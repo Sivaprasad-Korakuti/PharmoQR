129a1dc924ff8f39d8443f45efaeebd3113dbc0c
Sivaprasad-Korakuti <ksivaprasad032@gmail.com>
2026-03-14 14:13:42 +0530

    Create axios.js

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: window.location.hostname === 'localhost' 
+    ? 'http://localhost:5000/api' 
+    : '/api'
+});
+
+export default API;
